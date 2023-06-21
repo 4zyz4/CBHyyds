04fa81ae63f7aa43f21a6fcfe410b5cdbccdd20a
4 ZYZ 4 <133503200+4zyz4@users.noreply.github.com>
2023-06-21 18:57:37 +0800

    Create find_way.py

---
+++ find_way.py
@@ -0,0 +1,20 @@
+map=[" OOOO",
+    " OOOO",
+    " OOOO",
+    "  OOO",
+    "     ",]
+
+def find_way(map,start,end,way=[],):
+    try:
+        if start[0]<0 or start[1]<0:
+            return []
+        if map[start[0]][start[1]] in "oO":
+            return []
+        if start==end:
+            return [way+[end]]
+        if start in way:
+            return []
+    except:
+        return []
+    return find_way(map,[start[0]+1,start[1]],end,way=way+[start])+find_way(map,[start[0],start[1]+1],end,way=way+[start])+find_way(map,[start[0]-1,start[1]],end,way=way+[start])+find_way(map,[start[0],start[1]-1],end,way=way+[start])
+print(find_way(map,[0,0],[4,4]))
